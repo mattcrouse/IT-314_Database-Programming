79b77b5dd21568b33da00b89a7c136fc998630aa
Matthew A. Crouse <102341630+mattcrouse@users.noreply.github.com>
2022-04-15 23:33:04 -0400

    Upload Week 11 PL/SQL assignment

--- Week 11 PLSQL2.sql
+++ Week 11 PLSQL2.sql
@@ -1,6 +1,10 @@
 SET SERVEROUTPUT ON SIZE 3000
 
-/*PROBLEM 10*/-----------------------------------------
+/*PROBLEM 10
+Create a procedure named DDPROJ_SP that retrieves project information for a specific project based on a project ID. 
+The procedure should have two parameters: one to accept a project ID value and another to return all data for the specified project.
+Use a record variable to have the procedure return all database column values for the selected project. Test the procedure with an anonymous block.*/
+
 create or replace PROCEDURE DDPROJ_SP(
     projid IN DD_PROJECT.idproj%TYPE,
     rec_proj OUT DD_PROJECT%ROWTYPE)
@@ -10,7 +14,9 @@ BEGIN
     FROM DD_PROJECT
     WHERE idproj = projid;
 END;
---
+
+--TESTING THE PROCEDURE
+
 DECLARE
     rec_proj DD_PROJECT%ROWTYPE; 
 BEGIN
@@ -23,7 +29,12 @@ DBMS_OUTPUT.PUT_LINE(rec_proj.projfundgoal);
 DBMS_OUTPUT.PUT_LINE(rec_proj.projcoord);
 END;
 
-/*PROBLEM 11*/------------------------------------------
+/*PROBLEM 11
+Create a procedure named DDPAY_SP that identifies whether a donor currently has an active pledge with monthly payments.
+A donor ID is the input to the procedure. Using the donor ID, the procedure needs to determine whether the donor has any currently active pledges 
+based on the status field and is on a monthly payment plan. If so, the procedure is to return the Boolean value TRUE. 
+Otherwise, the value FALSE should be returned. Test the procedure with an anonymous block.*/
+
 create or replace PROCEDURE DDPAY_SP(
     donorid IN DD_PLEDGE.iddonor%TYPE,
     active OUT BOOLEAN)
@@ -44,7 +55,9 @@ BEGIN
         END IF;
     END LOOP;
 END;
---
+
+--TESTING THE PROCEDURE
+
 DECLARE 
     active BOOLEAN;
 BEGIN
@@ -57,7 +70,16 @@ BEGIN
    END IF;
 END;       
 
-/*PROBLEM 12*/
+/*PROBLEM 12
+Create a procedure named DDCKPAY_SP that confirms whether a monthly pledge payment is the correct amount. 
+The procedure needs to accept two values as input: a payment amount and a pledge ID.
+Based on these inputs, the procedure should confirm that the payment is the correct monthly increment amount, based on pledge data in the database.
+If it isn’t, a custom Oracle error using error number 20050 and the message “Incorrect payment amount - planned payment = ??” should be raised.
+The ?? should be replaced by the correct payment amount. The database query in25 the procedure should be formulated so that no rows are returned if the pledge
+isn’t on a monthly payment plan or the pledge isn’t found. If the query returns no rows, the procedure should display the message “No payment information.” 
+Test the procedure with the pledge ID 104 and the payment amount $25. Then test with the same pledge ID but the payment amount $20. 
+Finally, test the procedure with a pledge ID for a pledge that doesn’t have monthly payments associated with it.*/
+
 create or replace PROCEDURE DDCKPAY_SP(
     payamt IN DD_PLEDGE.pledgeamt%TYPE,
     pledgeid IN DD_PLEDGE.idpledge%TYPE)
